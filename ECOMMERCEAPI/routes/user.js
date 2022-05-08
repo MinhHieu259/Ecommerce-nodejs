ec5e57ea81a0cfa413fa07f8e352097c0557f4a9
MinhHieu259 <mhieu7252@gmail.com>
2022-05-08 14:56:58 +0700

    add jwt

--- ECOMMERCEAPI/routes/user.js
+++ ECOMMERCEAPI/routes/user.js
@@ -1,5 +1,23 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+    }
 
+    try {
+        const updateUser = await User.findByIdAndUpdate(req.params.id, 
+        {
+            $set: req.body      
+        }, 
+        {new: true});
+        res.status(200).json(updateUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router
